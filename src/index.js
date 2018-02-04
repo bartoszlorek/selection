0431a79b75de3c6ec97d0135c6757bb3fbec9edb
Bartosz Lorek <bery.lorek@gmail.com>
2018-02-04 17:25:16 +0100

    add utilities

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import selectionRange from './selection-range'
 import rangeContent from './range-content'
 
+import isTextElement from './is-text-element'
+import isTextNode from './is-text-node'
+import nodeValue from './node-value'
+
 module.exports = {
     selectionRange,
-    rangeContent
+    rangeContent,
+    isTextElement,
+    isTextNode,
+    nodeValue
 }
