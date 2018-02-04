07ebcdaea2d0ab39a6ae8ca975a1540312f7e6ec
Bartosz Lorek <bery.lorek@gmail.com>
2018-02-04 15:52:45 +0100

    returns content of nodes in range

---
+++ src/range-content.js
@@ -0,0 +1,25 @@
+import rangeNodes from './range-nodes'
+import nodeValue from './node-value'
+
+function rangeContent(range) {
+    return rangeNodes(range).map((node, index, nodes) => {
+        let text = nodeValue(node),
+            startOffset = 0,
+            endOffset = text.length
+
+        if (index === 0) {
+            startOffset = range.startOffset
+        }
+        if (index === nodes.length - 1) {
+            endOffset = range.endOffset
+        }
+        return {
+            node,
+            startOffset,
+            endOffset,
+            text
+        }
+    })
+}
+
+export default rangeContent
