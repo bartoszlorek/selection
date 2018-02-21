dd5c5330bb8a78deebf7c498f1b53d9cb93fd3d9
Bartosz Lorek <bery.lorek@gmail.com>
2018-02-21 18:16:39 +0100

    leaves range starting and ending with Text Nodes

---
+++ src/.internal/trim-to-text-nodes.js
@@ -0,0 +1,31 @@
+import { nextNode, prevNode } from '../node-sibling'
+import isTextNode from '../is-text-node'
+import getAncestor from './get-ancestor'
+
+function trimToTextNodes(range) {
+    let { startContainer, startOffset, endContainer, endOffset } = range
+
+    if (!isTextNode(startContainer)) {
+        startContainer = nextNode(startContainer, 3)
+        startOffset = 0
+    }
+    if (!isTextNode(endContainer)) {
+        endContainer = prevNode(endContainer, 3) || startContainer
+        endOffset = endContainer.nodeValue.length
+    }
+
+    return {
+        commonAncestorContainer: getAncestor(
+            startContainer,
+            endContainer
+        ),
+        collapsed: startContainer === endContainer
+                && startOffset === endOffset,
+        startContainer,
+        startOffset,
+        endContainer,
+        endOffset
+    }
+}
+
+export default trimToTextNodes
