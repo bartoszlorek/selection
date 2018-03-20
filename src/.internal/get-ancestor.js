41d7423e6e86bab1eeb3a199a60ce2cdbae6a40a
Bartosz Lorek <bery.lorek@gmail.com>
2018-03-20 01:23:12 +0100

    correct ancestor of two elements

--- src/.internal/get-ancestor.js
+++ src/.internal/get-ancestor.js
@@ -2,9 +2,17 @@ function getAncestor(start, end) {
     if (start === end) {
         return start
     }
+    let parentStart = start.parentElement
+    if (parentStart.contains(end)) {
+        return parentStart
+    }
+    let parentEnd = end.parentElement
+    if (parentEnd.contains(start)) {
+        return parentEnd
+    }
     return getAncestor(
-        start.parentElement || document.body,
-        end.parentElement || document.body
+        parentStart,
+        parentEnd
     )
 }
 
