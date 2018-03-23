fc693e4b9fbd696de29ed80fe7d7882bc5c7ea75
ubery <bery.lorek@gmail.com>
2018-03-24 00:29:17 +0100

    handles more specific range and find proper child

--- src/set-selection.js
+++ src/set-selection.js
@@ -1,18 +1,30 @@
 import getWindow from './.internal/get-window'
 import getDocument from './.internal/get-document'
 import getTagName from './.internal/get-tag-name'
+import getChildBy from './.utils/get-child-by'
 
-function setSelection(node, start = 0, end = -1) {
+const childWithContent = getChildBy(
+    e => e.textContent && e.textContent.trim() !== ''
+)
+
+function setSelection(node, start, end) {
     if (node == null) {
         return false
     }
-    if (end < start) {
+    if (start == null || start < 0) {
+        start = 0
+    }
+    if (end == null || end < start) {
         end = start
     }
 
     let tagName = getTagName(node)
     if (tagName === 'textarea' || tagName === 'input') {
-        node.setSelectionRange(start, end)
+        let length = node.value.length
+        node.setSelectionRange(
+            Math.min(start, length),
+            Math.min(end, length)
+        )
         return true
     }
 
@@ -21,20 +33,18 @@ function setSelection(node, start = 0, end = -1) {
             range = getDocument(node).createRange(),
             length = node.nodeValue.length
 
-        if (end > length) {
-            end = length
-            if (start > length) {
-                start = length
-            }
-        }
-        range.setStart(node, start)
-        range.setEnd(node, end)
+        range.setStart(node, Math.min(start, length))
+        range.setEnd(node, Math.min(end, length))
         selection.removeAllRanges()
         selection.addRange(range)
         return true
     }
 
-    return setSelection(node.firstChild, start, end)
+    return setSelection(
+        childWithContent(node),
+        start,
+        end
+    )
 }
 
 export default setSelection
