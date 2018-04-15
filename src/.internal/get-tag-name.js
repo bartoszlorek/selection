8db3fd640b2770eedaac9734a6a9cf9a0fcde289
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-16 01:44:47 +0200

    handles undefined element

--- src/.internal/get-tag-name.js
+++ src/.internal/get-tag-name.js
@@ -1,5 +1,11 @@
-function getTagName(element) {
-    return (element.tagName && element.tagName.toLowerCase()) || 'node'
+function getTagName(elem) {
+    if (elem == null) {
+        return ''
+    }
+    if (elem.tagName !== undefined) {
+        return elem.tagName.toLowerCase()
+    }
+    return 'node'
 }
 
 export default getTagName
