1a2ce1832e67060717387b564d0c8510c35b4290
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-17 12:06:07 +0200

    clean code

--- src/set-selection.js
+++ src/set-selection.js
@@ -5,9 +5,14 @@ import getChildBy from './.utils/get-child-by'
 import isNode from './.utils/is-node'
 import isRange from './.utils/is-range'
 
+const limit = (value, max) => (value < max ? value : max)
+
 const childWithContent = getChildBy(
     e => e.textContent && e.textContent.trim() !== ''
 )
+const closestNode = node => {
+    return node && (isNode(node) ? node : childWithContent(node))
+}
 
 function baseNodeSelection(startNode, endNode, start, end) {
     if (startNode == null) {
@@ -16,17 +21,12 @@ function baseNodeSelection(startNode, endNode, start, end) {
     if (endNode == null) {
         endNode = startNode
     }
-    let selection = nodeWindow(startNode).getSelection(),
-        range = nodeDocument(startNode).createRange(),
-        startLength = startNode.nodeValue.length,
-        endLength = endNode.nodeValue.length
+    start = limit(start, startNode.nodeValue.length)
+    end = limit(end, endNode.nodeValue.length)
 
-    if (start > startLength) {
-        start = startLength
-    }
-    if (end > endLength) {
-        end = endLength
-    }
+    let selection = nodeWindow(startNode).getSelection(),
+        range = nodeDocument(startNode).createRange()
+
     range.setStart(startNode, start)
     range.setEnd(endNode, end)
     selection.removeAllRanges()
@@ -36,8 +36,8 @@ function baseNodeSelection(startNode, endNode, start, end) {
 function baseElementSelection(elem, start, end) {
     let length = elem.value.length
     elem.setSelectionRange(
-        start < length ? start : length,
-        end < length ? end : length
+        limit(start, length),
+        limit(end, length)
     )
 }
 
@@ -69,13 +69,12 @@ function setSelection(node, start, end) {
         endNode = node.endContainer
         node = node.startContainer
     }
-    if (!isNode(node)) {
-        node = childWithContent(node)
-    }
-    if (endNode != null && !isNode(endNode)) {
-        endNode = childWithContent(endNode)
-    }
-    return baseNodeSelection(node, endNode, start, end)
+    return baseNodeSelection(
+        closestNode(node),
+        closestNode(endNode),
+        start,
+        end
+    )
 }
 
 export default setSelection
